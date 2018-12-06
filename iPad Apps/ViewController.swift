f2ca485b29a6da83bf9e14b60e6788b0aeebb4b5
Alphonso84 <Alphonso.SensleyJR@gmail.com>
2018-12-06 03:33:46 -0800

    Initial Commit

---
+++ iPad Apps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iPad Apps
+//
+//  Created by user on 12/6/18.
+//  Copyright © 2018 Alphonso Sensley II. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
