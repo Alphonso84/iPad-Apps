17a071c350a78f0474e4b81c84ba94f56bb11162
Alphonso84 <Alphonso.SensleyJR@gmail.com>
2018-12-07 02:35:04 -0800

    initial commit

--- iPad Apps/ViewController.swift
+++ iPad Apps/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var backGroundImage = UIImageView()
+    var square = UIView()
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        backGroundImage.contentMode = .scaleAspectFill
+        backGroundImage.image = UIImage(named: "backgroundImage")
+        backGroundImage.frame = self.view.bounds
+        
+        
+        square.layer.cornerRadius = 100
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        square.backgroundColor = .blue
+        square.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        UIView.animate(withDuration: 3) {
+            self.square.frame = CGRect(x: 300, y: 400, width: 200, height: 200)
+            self.square.backgroundColor = .yellow
+        }
+    
+        view.addSubview(backGroundImage)
+        view.addSubview(square)
+        
+        
+        
     }
-
-
 }
-
